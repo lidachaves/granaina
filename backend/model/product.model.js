c56f5b6aab0ff83bb6c76fca5ecaa7a77c610f45
KevinRodriguezC <132350848+KevinRodriguezC@users.noreply.github.com>
2024-04-10 12:57:30 +0200

    Create products model file

---
+++ backend/model/product.model.js
@@ -0,0 +1,27 @@
+async function getProductByURLName(URLName) {
+  return { name: "product 1", description: "Lorem ipsum", price: 20.0 };
+}
+
+async function createProduct(sellerId, URLName, name, description, price) {
+  return {
+    _id: "12345678",
+    name: "product 1",
+    description: "Lorem ipsum",
+    price: 20.0,
+  };
+}
+
+async function updateProduct(id, URLName, name, description, price) {
+  return true;
+}
+
+async function destroyProductByURLName(URLName) {
+  return true;
+}
+
+module.exports = {
+  getProductByURLName,
+  createProduct,
+  updateProduct,
+  destroyProduct,
+};
