133c7155a7b67da7a703b697edf14367d5c0b0e0
KevinRodriguezC <132350848+KevinRodriguezC@users.noreply.github.com>
2024-04-10 12:56:38 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const product = require("./routes/product");
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use("/api/", product);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
