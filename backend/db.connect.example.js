78276402a386e532cc2390f329d3ab4bda3b712b
KevinRodriguezC <132350848+KevinRodriguezC@users.noreply.github.com>
2024-04-10 13:40:25 +0200

    Add db.connect.js

---
+++ backend/db.connect.example.js
@@ -0,0 +1,6 @@
+// Haz una copia de este archivo con el nombre "db.connect.js" e
+// introduce en la constante databaseURL la url de tu base de datos.
+
+const databaseURL = "mongodb://localhost:27017";
+
+module.exports = { databaseURL };
