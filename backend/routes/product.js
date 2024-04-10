e10d9bac69118734853c06eaa75881392f74cbad
KevinRodriguezC <132350848+KevinRodriguezC@users.noreply.github.com>
2024-04-10 12:56:49 +0200

    Create products routes file

---
+++ backend/routes/product.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+router = express.Router();
+
+productController = require("../controller/product.controller");
+
+router.get("/:product", productController.get);
+
+router.post("/:product", productController.post);
+
+router.put("/:product", productController.put);
+
+router.delete("/:product", productController.destroy);
+
+module.exports = router;
