b5cd19dc938ad2eec54306125360b502c75a1e84
KevinRodriguezC <132350848+KevinRodriguezC@users.noreply.github.com>
2024-04-11 10:42:15 +0200

    Add database name

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-const databaseURL = require("./db.connect");
+const { databaseURL, databaseName } = require("./db.connect");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(databaseURL)
+  .connect(databaseURL, { dbName: databaseName })
   .then(() => {
     console.log("Connected to the database.");
 
