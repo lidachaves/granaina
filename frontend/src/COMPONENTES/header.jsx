71fc8d43cf2d74d6abfae0056f3aec096756467d
KevinRodriguezC <132350848+KevinRodriguezC@users.noreply.github.com>
2024-05-02 13:44:31 +0200

    Update header

--- frontend/src/COMPONENTES/header.jsx
+++ frontend/src/COMPONENTES/header.jsx
@@ -16,15 +16,15 @@ const Header = ({ toggleLogin }) => {
   };
 
   return (
-    <header className="bg-gray-800 text-white">
-      <div className="flex items-center justify-between py-3 px-5 mx-auto max-w-7xl">
+    <header className="bg-gray-800 text-white flex px-5 py-3 justify-center">
+      <div className="max-w-7xl w-full flex justify-between gap-5">
         <h1 className="text-xl font-bold">Logo</h1>
         <nav className="flex items-center space-x-9">
           <ul className="flex space-x-9">
-            <li><a href="#" className="hover:text-gray-300">Home</a></li>
-            <li><a href="#" className="hover:text-gray-300">About</a></li>
-            <li><a href="#" className="hover:text-gray-300">Categories</a></li>
-            <li><a href="#" className="hover:text-gray-300">Promotions</a></li>
+            <li><Link href="#" className="hover:text-gray-300">Home</Link></li>
+            <li><Link href="#" className="hover:text-gray-300">About</Link></li>
+            <li><Link href="#" className="hover:text-gray-300">Categories</Link></li>
+            <li><Link href="#" className="hover:text-gray-300">Promotions</Link></li>
           </ul>
           <div className="flex items-center space-x-5">
             {/* Icono de usuario */}
@@ -34,14 +34,6 @@ const Header = ({ toggleLogin }) => {
           </div>
         </nav>
       </div>
-      {showModal && (
-        <div className="fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 flex justify-center items-center">
-          <div className="bg-white rounded-lg p-8">
-            {/* Pasamos la función handleCloseModal al componente Login */}
-            <Login onClose={handleCloseModal} />
-          </div>
-        </div>
-      )}
     </header>
   );
 };
