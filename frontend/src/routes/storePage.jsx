0e3825a95c33fdd3cb39817cfb0da7ab48ae4d94
KevinRodriguezC <132350848+KevinRodriguezC@users.noreply.github.com>
2024-05-13 13:57:24 +0200

    Add store verification text

--- frontend/src/routes/storePage.jsx
+++ frontend/src/routes/storePage.jsx
@@ -4,41 +4,42 @@ import ProductCard from "../COMPONENTES/tarjeta.jsx";
 import Header from "../COMPONENTES/header.jsx";
 import { useParams } from "react-router-dom";
 
-
 function ProductPage() {
-    const { URLName } = useParams()
-    const [storeInfo, setStoreInfo] = useState(null)
+  const { URLName } = useParams();
+  const [storeInfo, setStoreInfo] = useState(null);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch("http://localhost:5000/api/users/" + URLName);
-                const json = await response.json()
-                if (response.ok) {
-                    setStoreInfo(json)
-                }
-            } catch (error) {
-                console.log(error)
-            }
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:5000/api/users/" + URLName
+        );
+        const json = await response.json();
+        if (response.ok) {
+          setStoreInfo(json);
         }
-        fetchData()
-    }, [])
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
 
-    return (
-        <div className="App">
-            <Header />
-            <div className="p-4">
-                {storeInfo ? (
-                    <div className="flex max-w-7xl mx-auto gap-4">
-                        <h2 className="text-2xl font-bold">
-                            {storeInfo.name}
-                        </h2>
-                    </div>
-                ) : "Loading"}
-
-            </div>
-        </div >
-    );
+  return (
+    <div className="App">
+      <Header />
+      <div className="p-4">
+        {storeInfo ? (
+          <div className="flex max-w-7xl mx-auto gap-4">
+            <h2 className="text-2xl font-bold">{storeInfo.name}</h2>
+            {storeInfo.verified && <h3>Tienda verificada</h3>}
+          </div>
+        ) : (
+          "Loading"
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default ProductPage;
