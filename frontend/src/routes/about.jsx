bb237e899cc7bc348b7590e032ab0743b56eba90
lidia <lidachaves@outlook.es>
2024-11-05 23:37:24 +0100

    add button
    add button

--- frontend/src/routes/about.jsx
+++ frontend/src/routes/about.jsx
@@ -121,12 +121,20 @@ function AboutUs() {
               <div className="flex flex-col rounded-lg border border-gray-100 px-4 py-8 text-center">
                 <dt className="order-last text-lg font-medium text-gray-500">
                   Total Sales
+                 
                 </dt>
+                
 
                 <dd className="text-4xl font-extrabold text-blue-600 md:text-5xl">
                   Articulos
                 </dd>
+                <button className="mt-4 w-auto mx-auto inline-flex items-center justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
+                  View More
+                </button>
+                
+                 
               </div>
+              
 
               <div className="flex flex-col rounded-lg border border-gray-100 px-4 py-8 text-center">
                 <dt className="order-last text-lg font-medium text-gray-500">
@@ -136,6 +144,9 @@ function AboutUs() {
                 <dd className="text-4xl font-extrabold text-blue-600 md:text-5xl">
                   Search
                 </dd>
+                <button className="mt-4 w-auto mx-auto inline-flex items-center justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
+                  View More
+                </button>
               </div>
 
               <div className="flex flex-col rounded-lg border border-gray-100 px-4 py-8 text-center">
@@ -146,6 +157,9 @@ function AboutUs() {
                 <dd className="text-4xl font-extrabold text-blue-600 md:text-5xl">
                  Decoora 
                 </dd>
+                <button className="mt-4 w-auto mx-auto inline-flex items-center justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
+                  View More
+                </button>
               </div>
             </dl>
           </div>
